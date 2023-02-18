f92d0da689a3bcd04ab0b798b026d1468e6408ef
Praveen Shinde <shindepraveen002@gmail.com>
2023-02-18 14:08:35 +0530

    added az-400 certificate and rearranged the positions

--- src/Components/Certification/Certification.jsx
+++ src/Components/Certification/Certification.jsx
@@ -10,13 +10,16 @@ const Certification = () => {
       </div>
       <div className="md:px-10">
         <CertificateBox
-          name="AZ-900: Microsoft Azure Fundamentals"
-          url="https://www.credly.com/badges/45b6a3cc-76ce-403b-ba8a-5b36af6519b9/public_url"
-        
+          name="Microsoft Certified: DevOps Engineer Expert          "
+          url="https://www.credly.com/badges/f1c522b2-ec44-4457-8ed2-5a84f1438d01/public_url"
         />
         <CertificateBox
-          name="AZ-204: Developing Solutions for Microsoft Azure"
+          name="Microsoft Certified: Azure Developer Associate"
           url="https://www.credly.com/badges/9bd66656-ced5-44e4-8e1b-e40c8110807d/public_url"
+        />
+        <CertificateBox
+          name="Microsoft Certified: Azure Fundamentals"
+          url="https://www.credly.com/badges/45b6a3cc-76ce-403b-ba8a-5b36af6519b9/public_url"
         />
       </div>
     </div>
